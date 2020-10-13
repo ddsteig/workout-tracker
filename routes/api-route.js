154720189b853d77651df025e8787ffce77166c3
Danny <tekhexrax@yahoo.com>
2020-10-12 20:23:09 -0400

    Finished routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -11,6 +11,34 @@ module.exports = (app) => {
       });
   });
 
-  
+  app.put("/api/workouts/:id", (req, res) => {
+    let workoutId = req.params.id;
+    Workout.findByIdAndUpdate(workoutId, { $push: { exercises: req.body } })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 
+  app.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
 };
--- routes/html-route.js
+++ routes/html-route.js
@@ -1,12 +1,15 @@
+const path = require("path")
+const root = path.join(__dirname, "../public");
+
 module.exports = function (app) {
     
     // Get method for exercise html
     app.get("/exercise", (req, res) => {
-        res
+        res.sendFile(root + "/exercise.html")
     });
   
     // Get method for stats html
     app.get("/stats", (req, res) => {
-        res
+        res.sendFile(root + "/stats.html")
     });
   };
